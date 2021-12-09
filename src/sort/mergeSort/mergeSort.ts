ce71fbf30143065a46b94a24e928e4263e9b0c75
cameron <me@you.com>
2021-12-09 10:20:26 +0000

    indents :)

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -1,9 +1,7 @@
 import { SortAlgorithm } from "../common";
 
 const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
-
     if (items.length <= 1) return items;
-
     let left = items.slice(0, items.length / 2);
     let right = items.slice(items.length / 2, items.length);
 
@@ -11,25 +9,19 @@ const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
     right = mergeSort(right);
 
     return merge(left, right);
-  
 }
 
 const merge = (left: any[], right: any[]) => {
     const sorted: any[] = [];
-
     while (left.length && right.length) {
       if (left[0] < right[0]) {
         sorted.push(left.shift());
-
-      } else {
+      } 
+      else {
         sorted.push(right.shift());
-
       }
-    
     }
-  
     return [...sorted, ...left, ...right];
-  
 }
    
 export default mergeSort;
